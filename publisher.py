8ce7d535b5350981cc139b81c4a962c820803142
maujza <maurojzalazar@gmail.com>
2023-10-23 07:16:41 -0300

    fixing nested structs

--- publisher.py
+++ publisher.py
@@ -7,10 +7,12 @@ import random
 # Define a global variable for the message key
 message_key = 0
 
+
 def generate_random_string(length):
     letters = string.ascii_letters
-    result_str = ''.join(random.choice(letters) for i in range(length))
+    result_str = "".join(random.choice(letters) for i in range(length))
     return result_str
+
 
 def random_message():
     global message_key  # Declare the variable as global so we can modify it
@@ -18,27 +20,39 @@ def random_message():
     # Increment the message key by 1
     message_key += 1
 
-    return json.dumps({
-        "key": message_key,
-        "value": random.random(),
-        "value_string": generate_random_string(10)
-    })
+    return json.dumps(
+        {
+            "customerid": "B3768689",
+            "datacontenttype": "application/json",
+            "id": "454364483-1\_002b0eab000092f80002",
+            "producttype": "SportsBook",
+            "source": "/SportsBook.Bet.Hydrate.Service",
+            "specversion": "1.0",
+            "subject": "BetEventResponseDto",
+            "time": "2023-08-25T11:53:13.2627129Z",
+            "traceid": "42735fd79e8d01d0b9f9d35132ca01f8",
+            "traceparent": "00-42735fd79e8d01d0b9f9d35132ca01f8-361f8e4dd0d54ce6-01",
+            "type": "bet-settled-reversal",
+            "Transaction": {
+                "BaseAmount": 0,
+                "CustomerAmount": 0,
+                "Date": "2023-08-25T11:53:13.2608732Z",
+            },
+        }
+    )
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq')
-)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
 channel = connection.channel()
 
 queue_name = "message_queue"
 channel.queue_declare(queue=queue_name)
 
 while True:
-    time.sleep(60)
+    time.sleep(5)
     message = random_message()
-    channel.basic_publish(
-        exchange='', routing_key=queue_name, body=message
-    )
+    channel.basic_publish(exchange="", routing_key=queue_name, body=message)
     print(" [x] Sent %r" % (message,))
-    
+
 
 connection.close()
